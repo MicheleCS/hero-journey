34021ed3e83f99841d6137c13cb504fcab15bfc0
MicheleCS <michelesouza-@outlook.com>
2023-11-24 15:53:15 -0300

    create search results

--- src/core/navbar/navbar.tsx
+++ src/core/navbar/navbar.tsx
@@ -1,53 +1,42 @@
-import { Link } from "react-router-dom";
-import { useNavbar } from "../context/navbarContext";
+import { Link, useNavigate } from "react-router-dom";
 import styles from './styles.module.css';
+import { useState } from 'react';
 
-
-const Navbar: React.FC = () => {
-  const { selected, searchQuery, setSearchQuery } = useNavbar();
-
-
+const Navbar = () => {
+  const [search, setSearch] = useState<string>('');
+  const navigate = useNavigate();
 
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchQuery(event.target.value);
+    setSearch(event.target.value);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (!search.trim()) return;
+
+    navigate(`/search?q=${encodeURIComponent(search)}`);
+    setSearch('');
   };
 
   return (
     <nav className={styles.navbar}>
       <h2>Battle Meta Human</h2>
-
-        <Link
-          className={selected === 'Home' ? 'active' : ''}
-          to={"/"}
-        >
-           <h3>HOME</h3>
-        </Link>
-        <Link
-          className={selected === 'Marvel' ? 'active' : ''}
-          to={"/marvel"}
-        >
-          <h3>MARVEL</h3>
-        </Link>
-        <Link
-          className={selected === 'DC' ? 'active' : ''}
-          to={"/dc"}
-        >
-          <h3>DC</h3>
-        </Link>
-        <Link
-          className={selected === 'Battle' ? 'active' : ''}
-          to={"/Battle"}
-        >
-          <h3>BATTLE</h3>
-        </Link>
-  
-      <input
-        type="text"
-        placeholder="Pesquisar Herói"
-        value={searchQuery}
-        onChange={handleSearchChange}
-      />
-      <button type="submit"></button>
+      <div className={styles.links}>
+        <Link to={"/"}><h3>HOME</h3></Link>
+        <Link to={"/marvel"}><h3>MARVEL</h3></Link>
+        <Link to={"/dc"}><h3>DC</h3></Link>
+        <Link to={"/battle"}><h3>BATTLE</h3></Link>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Search Meta Human"
+          value={search}
+          onChange={handleSearchChange}
+        />
+        <button type="submit">submit</button>
+      </form>
     </nav>
   );
 };
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,10 @@ import Navbar from '../core/navbar/navbar';
 import Home from '../features/home';
 import { DC, Marvel } from '../features/publisher';
 import Battle from '../features/battle';
+import { SearchResults } from '../features/search';
+
+
+
 
 export const RoutesThree = () => {
   return (
@@ -13,6 +17,8 @@ export const RoutesThree = () => {
         <Route path="/marvel" element={<Marvel/>} />
         <Route path="/dc" element={<DC/>} />
         <Route path="/battle" element={<Battle/>} />
+        <Route path="/search" element={<SearchResults/>} />
+
       </Routes>
     </>
   );
